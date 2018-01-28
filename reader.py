7fe815fc426e958ad2e9c6a84fcc033bf803db77
Jackson Pranica <jpranica@gmail.com>
2018-01-27 23:03:07 -0600

    Added function to reader file that scrapes the front page of achievement hunter's website for recent videos

--- reader.py
+++ reader.py
@@ -4,6 +4,8 @@ and a Youtube channel 'videos' page.
 """
 
 import datetime
+import json
+
 import requests
 from bs4 import BeautifulSoup
 
@@ -22,7 +24,7 @@ def latest_youtube(channel, size=5):
     # Grab the HTML from the channel page and make it easily readable for python
     page_html = requests.get(channel)
     html_text = page_html.text
-    soup = BeautifulSoup(html_text)
+    soup = BeautifulSoup(html_text, 'lxml')
 
     channel_name = soup.find_all('title')[0].text.split()[0]
     video_list = soup.find_all('div', {"class": "yt-lockup-content"})[0:size]
@@ -39,4 +41,38 @@ def latest_youtube(channel, size=5):
         created_at.append(datetime.datetime.now())
 
     return {"name": channel_name, "title": title, "url": url,
+            "uploaded": date_uploaded, "created": created_at}
+
+def latest_achievement_hunter(size=5):
+    """
+    Pulls the latest videos from the achievement hunter website
+    filtering out a few of my least favorites
+    """
+
+    #Initialize variables
+    title = []
+    url = []
+    date_uploaded = []
+    created_at = []
+
+    # Grab the html from the website and make it easily readable by python
+    page_html = requests.get('http://achievementhunter.roosterteeth.com/')
+    html_text = page_html.text
+    soup = BeautifulSoup(html_text, "lxml")
+
+    video_list = json.loads(str(soup.find(id="recent-carousel-comment").contents[1]))
+
+    not_wanted_videos = ["Off Topic", "AHWU", "Last Call", "Let's Watch", "Shenanigans",
+                         "Heroes & Halfwits", "Heroes & Halfwits Post Show"]
+
+    filtered_video_list = [video for video in video_list if video["show_name"]
+                           not in not_wanted_videos]
+
+    for video in filtered_video_list[0:min([len(filtered_video_list), size])]:
+        title.append(video["title"])
+        url.append(video["url"])
+        date_uploaded.append(video["published_at"]["date"])
+        created_at.append(datetime.datetime.now())
+
+    return {"name": "Achievement Hunter", "title": title, "url": url,
             "uploaded": date_uploaded, "created": created_at}
