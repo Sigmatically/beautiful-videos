b9635970a1dbc2e6b6b26d953baf7b667d419416
Jackson Pranica <jpranica@gmail.com>
2018-02-05 20:52:49 -0600

    Added list support for calls to Youtube Channel reader

--- reader.py
+++ reader.py
@@ -9,11 +9,14 @@ import json
 import requests
 from bs4 import BeautifulSoup
 
-def latest_youtube(channel, size=5):
+def latest_youtube(channels, size=5):
     """
     Takes a Youtube channel link or list of Youtube channel links and pulls out a specified
     amount of the most recent videos from those pages
     """
+
+    if isinstance(channels, str):
+        channels = [channels]
 
     # Initiate variables
     title = []
@@ -21,26 +24,28 @@ def latest_youtube(channel, size=5):
     date_uploaded = []
     created_at = []
 
-    # Grab the HTML from the channel page and make it easily readable for python
-    page_html = requests.get(channel)
-    html_text = page_html.text
-    soup = BeautifulSoup(html_text, 'lxml')
+    # Iterating over a list of channel names
+    for channel in channels:
+        # Grab the HTML from the channel page and make it easily readable for python
+        page_html = requests.get(channel)
+        html_text = page_html.text
+        soup = BeautifulSoup(html_text, 'lxml')
 
-    channel_name = soup.find_all('title')[0].text.split()[0]
-    video_list = soup.find_all('div', {"class": "yt-lockup-content"})[0:size]
+        channel_name = soup.find_all('title')[0].text.split()[0]
+        video_list = soup.find_all('div', {"class": "yt-lockup-content"})[0:size]
 
-    for video in video_list:
-        # Seperate the video information
-        video_attrs = video.contents[1].contents[0]
-        video_upload = video.contents[3].find_all('li')[1].text
+        for video in video_list:
+            # Seperate the video information
+            video_attrs = video.contents[1].contents[0]
+            video_upload = video.contents[3].find_all('li')[1].text
 
-        # Add to container lists
-        title.append(video_attrs["title"])
-        url.append('http://www.youtube.com/' + video_attrs["href"])
-        date_uploaded.append(video_upload)
-        created_at.append(datetime.datetime.now())
+            # Add to container lists
+            title.append(video_attrs["title"])
+            url.append('http://www.youtube.com/' + video_attrs["href"])
+            date_uploaded.append(video_upload)
+            created_at.append(datetime.datetime.now())
 
-    return {"name": channel_name, "title": title, "url": url,
+    return {"name": "YouTube", "title": title, "url": url,
             "uploaded": date_uploaded, "created": created_at}
 
 def latest_achievement_hunter(size=5):
